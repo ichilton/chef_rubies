5d5513f008e15d152c626d5219d5968cf2f9228f
Ian Chilton <ian@ichilton.co.uk>
2013-10-11 16:50:10 +0000

    Initial Implementation

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: rubies
+# Attributes:: default
+#
+
+default[:rubies][:list] = ['ruby 1.9.3-p194', 'ruby 2.0.0-p195']
+
+default[:rubies][:install_bundler] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,16 @@
 # Cookbook Name:: rubies
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
+
+include_recipe 'chruby_install'
+include_recipe 'ruby_install'
+
+node[:rubies][:list].each do |ruby|
+  ruby_install_ruby ruby
+
+  if node[:rubies][:install_bundler]
+    gem_package "bundler" do
+      gem_binary("/opt/rubies/#{ruby.gsub(' ', '-')}/bin/gem")
+    end
+  end
+end
