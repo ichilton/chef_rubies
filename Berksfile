83e4f0fb342a6ea1de13c03f6b86c6ca47db5f40
Ian Chilton <ian@ichilton.co.uk>
2013-10-11 16:50:23 +0000

    Initial Berksfile & metadata.rb

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 site :opscode
 
+cookbook "chruby_install", github: "ichilton/chef_chruby_install"
+
 metadata
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license          'All rights reserved'
 description      'Installs different Ruby versions, chruby for switching and any gems required'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'chruby_install'
+depends 'ruby_install'
+
+supports 'ubuntu'
