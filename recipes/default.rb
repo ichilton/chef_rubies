a4959ba9efdb52bad60e853f283b21443c3b6da4
Stephen Lauck <stephen.lauck@gmail.com>
2014-09-24 10:34:58 -0400

    add support for specifying bundler version

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,9 +10,10 @@ include_recipe 'ruby_install'
 node[:rubies][:list].each do |ruby|
   ruby_install_ruby ruby
 
-  if node[:rubies][:install_bundler]
+  if node[:rubies][:bundler][:install]
     gem_package "bundler" do
       gem_binary("/opt/rubies/#{ruby.gsub(' ', '-')}/bin/gem")
+      version node[:rubies][:bundler][:version]
     end
   end
 end
